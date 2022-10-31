fd6736ef0992e42f1ee9606f76c77e11703a7ec0
Ed Honour <40154656+edwardhonour@users.noreply.github.com>
2022-10-31 18:43:46 -0500

    This is a mess

--- src/app/pages/instructions-page/instructions-page.component.ts
+++ src/app/pages/instructions-page/instructions-page.component.ts
@@ -1,26 +1,70 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { DataService } from '../../data.source/data.source.module';
 import { UntypedFormBuilder } from '@angular/forms';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, AbstractControl } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { NgLocalization } from '@angular/common';
-import { Editor } from 'ngx-editor';
+import { Editor, Toolbar } from 'ngx-editor';
+
+import jsonDoc from '../doc';
 
 @Component({
   selector: 'app-instructions-page',
   templateUrl: './instructions-page.component.html',
-  styleUrls: ['./instructions-page.component.css']
+  styleUrls: ['./instructions-page.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 
 export class InstructionsPageComponent implements OnInit {
 
+  editordoc = jsonDoc;
+  isCopy = false;
+  myInput:any;
+
+  @HostListener('click', ['$event.target']) onClick(e: any){
+    console.log(e);
+    if (this.isCopy) {
+      e.innerText = e.innerText + this.myInput
+    }
+  }
+
+  editor: any;
+  editor2: any;
+  editor3: any;
+  editorContent3: any;
   data: any;
   p: any;
   user: any;
   history:any;
   menu:any;
-  editor: Editor;
+
+  html: any;
+
+
+  toolbar: Toolbar = [
+    ['bold', 'italic'],
+    ['underline', 'strike'],
+    ['code', 'blockquote'],
+    ['ordered_list', 'bullet_list'],
+    [{ heading: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'] }],
+    ['link', 'image'],
+    ['text_color', 'background_color'],
+    ['align_left', 'align_center', 'align_right', 'align_justify'],
+  ];
+
+  form = new FormGroup({
+    editorContent: new FormControl(
+      { value: jsonDoc, disabled: false },
+    ),
+    editorContent2: new FormControl(
+      { value: jsonDoc, disabled: false },
+    ),
+  });
+
+  //get doc(): AbstractControl {
+  //  return this.form.get('editorContent');
+  //}
 
   constructor(
     private _activatedRoute: ActivatedRoute,
@@ -32,7 +76,11 @@ export class InstructionsPageComponent implements OnInit {
 
   ngOnInit(): void {
     this.history='N';
+    this.editorContent3=jsonDoc;
     this.editor = new Editor();
+    this.editor2 = new Editor();
+    this.editor3 = new Editor();
+
     this._activatedRoute.data.subscribe(({ 
       data })=> { 
         this.data=data;
@@ -45,5 +93,14 @@ export class InstructionsPageComponent implements OnInit {
       this.user=data.user;
     }) 
 
+  }
+  
+  onChange(html: object) {
+ 
+  }
+
+  doExec() {
+    let payLoad = JSON.stringify(this.form.value);
+    console.log(payLoad);
   }
 }
